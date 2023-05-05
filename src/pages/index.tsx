6675bf7c7d06d64ab0bcd9baf3e79871e9544b2e
rykerFeng <2279549769@qq.com>
2023-05-05 14:34:54 +0800

    feat: 新增tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,7 @@
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
-
 export default function Home() {
   return (
-    <>
-      Hello World
-    </>
-  );
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
 }
